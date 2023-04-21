5872475dd423b8b31cf323d7fa43191f2a58d0e6
alexdtm9 <a.m.talpau@gmail.com>
2023-04-22 01:50:29 +0200

    Create DB and wire up GraphQL

---
+++ src/CommanderGQL/GraphQL/Query.cs
@@ -0,0 +1,9 @@
+using CommanderGQL.Data;
+using CommanderGQL.Models;
+
+namespace CommanderGQL.GraphQL;
+
+public class Query
+{
+    public IQueryable<Platform> GetPlatform([Service] AppDbContext context) => context.Platforms;
+}
--- src/CommanderGQL/Program.cs
+++ src/CommanderGQL/Program.cs
@@ -1,6 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using CommanderGQL.Data;
+using CommanderGQL.GraphQL;
+using Microsoft.EntityFrameworkCore;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer
+    (builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddGraphQLServer()
+    .AddQueryType<Query>();
+
+var app = builder.Build();
+app.MapGraphQL();
 
 app.Run();
