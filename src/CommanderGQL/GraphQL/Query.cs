c49cd29221284a5e5cfbb78bb4c38b18d3e0ff9e
alexdtm9 <a.m.talpau@gmail.com>
2023-04-22 20:52:48 +0200

    Add commands query

--- src/CommanderGQL/GraphQL/Query.cs
+++ src/CommanderGQL/GraphQL/Query.cs
@@ -7,4 +7,7 @@ public class Query
 {
     [UseProjection]
     public IQueryable<Platform> GetPlatform(AppDbContext context) => context.Platforms;
+    
+    [UseProjection]
+    public IQueryable<Command> GetCommand(AppDbContext context) => context.Commands;
 }
